10603b19bd32cf5248618db30b8e2949da5a74fb
Muneeb-Shahabhassan <muneebsahabhassan098890@gmail.com>
2024-06-03 11:44:19 -0700

    12.READING NAMES

---
+++ app.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let members = [`Azam`, `Muneeb`, `Maroof`, `Mubashir`, `Zaid`];
+let massage = `Kal shadi ha kis ki ? `;
+for (let i = 0; i < members.length; i++) {
+    console.log(massage + members[i]);
+}
---
+++ app.ts
@@ -0,0 +1,6 @@
+let members : string[] =[`Azam`,`Muneeb`,`Maroof`,`Mubashir`,`Zaid`];
+let massage: string=`Kal shadi ha kis ki ? `;
+ for( let i=0; i<members.length; i++){
+    console.log(massage + members[i]);
+    
+ }
